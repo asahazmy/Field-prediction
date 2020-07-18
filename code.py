9f348b0065b7a729b680a18237872bfc63d5c95a
asahazmy <60892162+asahazmy@users.noreply.github.com>
2020-07-18 21:17:45 +0700

    Update code.py

--- code.py
+++ code.py
@@ -11,7 +11,8 @@ import numpy as np
 import os
 import cv2
 
-# Configurations
+# Kernel Configurations
+make_submission = False # used to turn off lengthy model analysis so a submission version doesn't run into memory error
 load_pretrained_model = True # load a pre-trained model
 save_model = True # save the model after training
 train_dir = '' # directory of training images
@@ -19,10 +20,9 @@ pretrained_model_path = '' # path of pretrained model
 model_save_path = '' # path of model to save
 train_image_dir = os.path.join(train_dir, 'train_images') #
 
-# resize image
+# original image is 1600x256, so we will resize it
 img_w = 800 # resized weidth
 img_h = 256 # resized height
-batch_size = 12
 epochs = 25
 # batch size for training unet
 k_size = 3 # kernel size 3x3
@@ -30,6 +30,63 @@ val_size = .20 # split of training set between train and validation set
 # we will repeat the images with lower samples to make the training process more fair
 repeat = False
 
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+
+'''input data & mask'''
+#Normalisasi data
+def normalize(input_image, input_mask):
+  input_image = tf.cast(input_image, tf.float32) / 255.0
+  input_mask -= 1
+  return input_image, input_mask
+
+#input data
+def load_image_train(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (256, 256))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], ((256, 256))
+
+  if tf.random.uniform()> 0.5:
+    input_image = tf.image.flip_left_right(input_image)
+    input_mask = tf.image.flip_left_right(input_mask)
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+def load_image_test(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+TRAIN_LENGTH = info.splits['train'].num_examples
+train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+test = dataset['test'].map(load_image_test)
+
+train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+test_dataset = test.batch(BATCH_SIZE)
+
+'''checking the data'''
+def display(display_list):
+  plt.figure(figsize=(15, 15))
+
+  title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+  for i in range(len(display_list)):
+    plt.subplot(1, len(display_list), i+1)
+    plt.title(title[i])
+    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+    plt.axis('off')
+  plt.show()
+
+for image, mask in train.take(4):
+  sample_image, sample_mask = image, mask
+display([sample_image, sample_mask])
 
 
 '''ResUnet'''
